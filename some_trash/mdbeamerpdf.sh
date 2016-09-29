ffdebe12f872f8f4c54cc4e32bb8ac898baa0d1a
jiri hubacek <jiri.hubacek@gmail.com>
2016-09-29 23:42:27 +0200

    Scripts for generating pdf added.
    Latex by vbatts/pandoc docker image.
    Beamer by vbatts/pandoc docker image.

---
+++ some_trash/mdbeamerpdf.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+##
+# This script convert file $1 to pdf by vbatts/pandoc docker image.
+#
+
+exec docker run --rm -v $(pwd)/$1:/input/in.pdf:ro -v $(pwd)/:/output/ vbatts/pandoc -f markdown_github -t beamer -o /output/out.pdf /input/in.pdf
