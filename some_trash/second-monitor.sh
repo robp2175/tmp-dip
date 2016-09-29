e5196f783c2f2da717447e1d007c37a55bddc5dc
jiri hubacek <jiri.hubacek@gmail.com>
2016-09-29 18:21:59 +0200

    Second monitor moved from D-Sub to HDMI.

--- some_trash/second-monitor.sh
+++ some_trash/second-monitor.sh
@@ -4,5 +4,5 @@
 # This script runs the second monitor on the right side, 90 dgree rotated.
 #
 
-xrandr --output eDP1 --auto --output DP1 --auto --right-of eDP1 --rotate left --brightness 0.8
+xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1 --rotate left --brightness 0.8
 feh --bg-center ~/pics/nagato.jpg
