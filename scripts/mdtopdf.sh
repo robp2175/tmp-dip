a9ce5e7c718b485dd415a2eeba2292723e8cb12a
jiri hubacek <jiri.hubacek@gmail.com>
2016-10-12 13:39:28 +0200

    This script is covered by `mdlatex.sh` script.

--- scripts/mdtopdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-##
-# This script convert file $1 to pdf by hairyhenderson/markdowntopdf docker image.
-#
-
-exec docker run --rm -v $(pwd)/$1:/in/in.md -v $(pwd):/out hairyhenderson/markdowntopdf
