2ef2a55cb9666c41de9b952792b1b20f43421729
jiri hubacek <jiri.hubacek@gmail.com>
2017-02-28 13:33:28 +0100

    Convert `rst` to `pdf` (text, presentation) with docker.

---
+++ scripts/rstlatexpdf.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+##
+# This script convert file $1 to pdf by vbatts/pandoc docker image.
+#
+
+exec docker run --rm -v $(pwd)/:/input/:ro -v $(pwd)/:/output/ vbatts/pandoc -f rst -t latex -o /output/out.pdf /input/$1
